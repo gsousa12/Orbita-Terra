a56a5d07b498e158d6cdf03be769730b767cfddf
Gabriel Sousa <wgsousa12@gmail.com>
2022-10-12 13:02:03 -0300

    add

---
+++ Modelo de Orbita da Terra/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Modelo_de_Orbita_da_Terra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Começar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pausar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(119, 278);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar1.Size = new System.Drawing.Size(190, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(163, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Velocidade da Terra";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(434, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Modelo de Orbita da Terra/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelo_de_Orbita_da_Terra
+{
+    public partial class Form1 : Form
+    {
+        // Váriaveis
+        float angle = 0, earthVelocity = 1;
+        int px = 210, py = 150, dst = 50;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value; // Setando o valor do Timer para o valor do Track
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate(); // Invalidar os gráficos se o timer for iniciado
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            StringFormat str = new StringFormat();
+            Graphics g = e.Graphics;
+
+            // Desenhar a Elipse da Terra
+            g.SmoothingMode = SmoothingMode.AntiAlias; // Método para desenhar gráficos suaves..
+            g.TranslateTransform(px,py,MatrixOrder.Append);
+            g.DrawEllipse(Pens.White, -dst + 3,-dst + 3, 100,100); // Desenhando uma elipse
+            g.ResetTransform();
+
+            // Desenhando o SOL
+            g.FillEllipse(Brushes.Yellow, px + 5, py - 10, 20, 20);
+
+            // Desenhando o texto "SOL" 
+            //g.DrawString("SOL",Font,new SolidBrush(Color.Yellow), (int)(px - dst + 50), (int)(py - dst + 25), str);
+
+            // Fazendo a animação da circulação da Terra (funções de seno e cosseno) 
+            int x = (int)(px + dst * Math.Sin(angle * Math.PI / 182.5f));
+            int y = (int)(py + dst * Math.Cos(angle * Math.PI / 182.5f));
+            g.FillEllipse(Brushes.DeepSkyBlue, (int)x, (int)y ,10 , 10);
+
+            angle -= earthVelocity;
+
+            // Desenhando a Elipse da Lua
+            g.DrawEllipse(Pens.White,(int)x - 10, (int)y - 10, 30, 30);
+
+            // Fazendo a animação da circulação da Lua (funções de seno e cosseno) 
+            int x1 = (int)(x + 15 * Math.Sin(angle * Math.PI / 30f));
+            int y1 = (int)(y + 15 * Math.Cos(angle * Math.PI / 30f));
+
+            g.FillEllipse(Brushes.LightGray, (int)x1, (int)y1, 8, 8);
+
+        }
+    }
+}
